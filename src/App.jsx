7b55fd2f443135f962bfce2d2ae8895a69c6fe57
Shane Sweeney <ssweeney@calamp.com>
2023-04-04 18:12:48 -0700

    Rename file, Add Client Provider, Add Search module, remove Counter module

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import header from './header.png';
+import './App.css';
+import Search from './features/search/Search';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+function App() {
+
+  // Create a client
+  const queryClient = new QueryClient()
+
+  return (
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          <img src={header} className="App-logo" alt="logo" />
+        </header>
+        <Search />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
