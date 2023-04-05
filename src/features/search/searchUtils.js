d70234f189c0df5ca7cd0bb11e227192ce7ada90
Shane Sweeney <ssweeney@calamp.com>
2023-04-05 15:21:09 -0700

    Add documentation to utilities

--- src/features/search/searchUtils.js
+++ src/features/search/searchUtils.js
@@ -1,3 +1,11 @@
+/**
+ * @resultCreators
+ * An object that contains three functions: events, venues, and performers. 
+ * Each function takes an item as an argument and returns an object with 
+ * properties id, image, title, and subtitle. These functions are used to 
+ * create consistent results objects for each type of item.
+ */
+
 const resultCreators = {
   events: (item) => {
     const performer = item.performers.find(p => p.primary) || item.performers[0];
@@ -29,6 +37,14 @@ const resultCreators = {
 const TOP_PICK = 'top_pick'
 const DEFAULT_RESULT_CREATOR = 'events'
 
+/**
+ * @mapResults
+ * @param items - array of search results
+ * A function that maps over an array of items and uses the resultCreators
+ * functions to create results objects based on the item's meta.display_group.
+ * 
+ * @returns array of non-null objects
+ */
 const mapResults = (items) => (
  items.map(item => {
     const resultCreator = resultCreators[item?.meta?.display_group] || resultCreators[DEFAULT_RESULT_CREATOR];
@@ -36,6 +52,18 @@ const mapResults = (items) => (
   }).filter(Boolean)
 )
 
+/**
+ * @sortResults
+ * @param data 
+ * A function takes in an object of search results from the API fetchSearch
+ * and sorts them based on their display groups, with a maximum of 3 results per group.
+ * Top picks are extrapolated from the other group types (venues, performers, events). 
+ * The sorted results are then returned as an array of objects, each containing information
+ * about the search result. 
+ * 
+ * @returns An array of objects containing information about the search results, 
+ * sorted by display groups with a maximum of 3 results per group.
+ */
 export const sortResults = (data) => {
   const {display_groups: groups, ...groupTypes} = data
 
