0b1856f527503858732794f9487060784990a949
Shane Sweeney <ssweeney@calamp.com>
2023-04-04 20:37:44 -0700

    Add SearchForm

---
+++ src/features/search/SearchForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BsSearch} from 'react-icons/bs';
+
+
+const SearchForm = ({handleSearch}) => {
+
+  return (
+    <div className="input-container">
+     <span>
+     <BsSearch className="icon" />
+      </span> 
+      <input
+        type="text"
+        name="search"
+        onChange={event => handleSearch(event.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchForm
